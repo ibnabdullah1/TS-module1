5677da1454e0a977483f441c97fb335093a72442
abdullah01 <arfathossen541@gmail.com>
2024-05-01 15:03:18 +0600

    module class1.8 completed

---
+++ src/1.8.ts
@@ -0,0 +1,23 @@
+{
+  //  Destructuring
+
+  const user = {
+    id: 123,
+    name: {
+      firstName: "arafat",
+      lastName: "Hosen",
+    },
+    contactNo: "01872384654",
+    address: "Dhaka",
+  };
+
+  const {
+    contactNo,
+    name: { lastName: lastN },
+  } = user;
+
+  const myFriends = ["chandler", "joey", "ross", "rachel"];
+
+  const [, , bestFriend] = myFriends;
+  // const [, , bestFriend] = myFriends;
+}
